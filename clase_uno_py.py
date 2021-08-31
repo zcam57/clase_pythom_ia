f9883a43926445a284d47f73358426097b338393
Mac Perez <macjorian.perez57@gmail.com>
2021-08-30 20:16:34 -0500

    tipos de datos, conversiones y concatenaciones

--- clase_uno_py.py
+++ clase_uno_py.py
@@ -7,12 +7,127 @@ Created on Mon Aug 30 19:02:39 2021
 
 print("hellor world")
 
-#variables
-a=3
+# Variables
+a = 3
 print(type(a))
-a="hola"
+a = "hola"
 print(type(a))
-a=3.5
+a = 3.5
 print(type(a))
-a=True
+a = True
 print(type(a))
+
+# Operaciones
+
+# Suma
+
+a = 5
+b = 2
+c = a + b
+print(c)
+
+#Resta
+
+a = 5
+b = 2
+c = a - b
+print(c)
+
+# Multiplicacion
+
+a = 5
+b = 2
+c = a * b
+print(c)
+
+# Division
+
+a = 5
+b = 2
+c = a / b
+print(c)
+
+a = 5
+b = 2
+c = a // b
+print(c)
+
+# Potencia
+
+a = 5
+b = 2
+c = a ** b
+print(c)
+
+# Raiz
+
+a = 27
+c = a ** (1/3)
+print(c)
+
+# import math
+# raiz = math.sqrt(25)
+# print(raiz)
+
+a = "hola mundo"
+a = 'Hola mundo'
+b = "I can´t do it"
+c = 'Alias "mac"'
+
+# Entero int
+
+a = 5
+
+# Decimal float
+
+a = 5.6
+
+# Booleano bool
+
+x = True
+y = False
+
+# Conversiones entre tipos de datos
+
+# Convertitr de x a entero
+
+a = '5'
+y = float(a)
+print(y)
+print(type(y))
+
+# Convertir de x a string
+
+a = 5
+y = str(a)
+print(y)
+print(type(y))
+
+# Concatenaciones
+
+a = 'hola'
+b = 'mundo'
+c = a + ' ' + b
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
