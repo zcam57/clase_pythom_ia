22b8a1d956c97d950f9bf07edd7e56a9792e80eb
Mac Perez <macjorian.perez57@gmail.com>
2021-08-30 19:30:23 -0500

    Variables y operaciones en pyton

--- clase_uno_py.py
+++ clase_uno_py.py
@@ -5,3 +5,14 @@ Created on Mon Aug 30 19:02:39 2021
 @author: cammac
 """
 
+print("hellor world")
+
+#variables
+a=3
+print(type(a))
+a="hola"
+print(type(a))
+a=3.5
+print(type(a))
+a=True
+print(type(a))
