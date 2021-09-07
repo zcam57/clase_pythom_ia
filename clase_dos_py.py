8407323c1bdfaf1e40daa9fb7cbe0257c18cc966
Mac Perez <macjorian.perez57@gmail.com>
2021-09-06 19:25:48 -0500

    Tabla de veradad, Jerarquia de operaciones y condiciones

--- clase_dos_py.py
+++ clase_dos_py.py
@@ -5,3 +5,53 @@ Created on Mon Sep  6 19:04:06 2021
 @author: cammac
 """
 
+# Condicionales
+
+# Tabla de verdad
+
+# Tabla de and
+# v and v = v
+# v and f = f
+# f and v = f
+# f and f = f
+
+print(True and True)  # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False)  # False
+
+# Tabla del or
+# v or v = v
+# v or f = v
+# f or v = v
+# f or f = f
+
+print(True or True)  # True
+print(True or False)  # True
+print(False or True)  # True
+print(False or False)  # False
+
+# Negacion
+
+print(not True)  # False
+print(not False)  # True
+
+# Mas de dos condiciones al mismo tiempo
+
+print(True and False and True or False or True or True)  # True
+print(True and (False and True) or False or (True or True))  # True
+
+# Jerarquia de operaciones
+# 1. parentesis y llaves
+# 2. Potencia y Raices
+# 3. Multiplicacion y Division
+# 4. Sunas y Restas
+
+# Jerarquia de operaciones booleanas
+# 1. Parantesis y llaves
+# 2. Tabla de verdad
+
+
+
+
+
