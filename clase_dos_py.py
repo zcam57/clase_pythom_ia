1d344cb4f1ca60e846525fc767489f1959656c8e
Mac Perez <macjorian.perez57@gmail.com>
2021-09-09 15:58:45 -0500

    punto #5 valor de casa y carro

--- clase_dos_py.py
+++ clase_dos_py.py
@@ -222,3 +222,26 @@ if(promedio > 170):
 elif(promedio <= 170):
     print('Su empresa cumple con el promedio de contaminación permitida ')
 valor = 0
+
+# 5 HUA Una persona se encuentra con un problema de comprar un automóvil
+# o un terreno, los cuales cuestan exactamente lo mismo. Sabe que
+# mientras el automóvil se devalúa, con el terreno sucede lo contrario.
+# Esta persona comprará el automóvil si al cabo de tres años la
+# devaluación de este no es mayor que la mitad del incremento del
+# valor del terreno. Ayúdale a esta pesona a determinar si debe o no
+# comprar el automóvil.
+
+valor = float(input('Ingrese el valor  ambos bienes: '))
+# devaluacion de un auto Consiste en dividir el valor
+# del auto en la vida útil del mismo.
+carro = valor / 3
+casa = (valor / 3) * 0.5
+if(carro <= casa):
+    print('Es recomendable comprar el carro  ')
+    print(f'ya que el carro en estos  3 años se devaluó menos del 50%  del valor que ganó la casa: ${carro: ,}')
+    print(f'El 50% del valor que ganó la casa en estos 3 años es de: ${casa: ,}')
+elif(carro > casa):
+    print('Es recomendable comprar la casa ')
+    print(f'ya que el carro en estos  3 años se devaluó mas del 50%  del valor que ganó la casa: ${carro: ,}')
+    print(f'El 50% del valor que ganó la casa en estos 3 años  es de: ${casa: ,}')
+
