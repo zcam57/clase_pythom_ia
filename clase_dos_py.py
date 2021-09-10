82a876a2b326863947b9a3b510f68bcd4a992c98
Mac Perez <macjorian.perez57@gmail.com>
2021-09-10 11:03:05 -0500

    punto #6 compra de computadores

--- clase_dos_py.py
+++ clase_dos_py.py
@@ -245,3 +245,24 @@ elif(carro > casa):
     print(f'ya que el carro en estos  3 años se devaluó mas del 50%  del valor que ganó la casa: ${carro: ,}')
     print(f'El 50% del valor que ganó la casa en estos 3 años  es de: ${casa: ,}')
 
+# 6 HUA En una fábrica de computadoras se planea ofrecer a los clientes un
+# descuento que dependerá del número de computadoreas que
+# compre. Si las computadoras son menos de cinco se les dará un 10%
+# de descuento sobre el total de la compra; si el número de
+# computadoras es mayor o igual a cinco pero menos de diez se le
+# otorga un 20% de descuento; y si son 10 o más se les da un 40%. El
+# precio de cada computadora es de $11.000.
+
+cantidad = int(input('Ingrese la cantidad de computadores que desea comprar '))
+precio = cantidad * 11000
+if(cantidad < 5 and cantidad > 0):
+    total = precio - (precio * 0.1)
+    print(f'El precio a pagar por la compra de; {cantidad} computadores es:${precio: ,}  - 10% de descuento  es de : ${total: ,}')
+elif(cantidad >= 5 and cantidad < 10):
+    total = precio - (precio * 0.2)
+    print(f'El precio a pagar por la compra de; {cantidad} computadores es:${precio: ,}  - 20% de descuento  es de : ${total: ,}')
+elif(cantidad >= 10 ):
+    total = precio - (precio * 0.4)
+    print(f'El precio a pagar por la compra de; {cantidad} computadores es:${precio: ,}  - 40% de descuento  es de : ${total: ,}')
+elif(cantidad <= 0):
+    print(f'La siguiente cantidad {cantidad} no es valida')
