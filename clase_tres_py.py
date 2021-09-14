6f962baf50a8dd1c7c1e2ac711d26331330eacd6
Mac Perez <macjorian.perez57@gmail.com>
2021-09-13 21:06:39 -0500

    Tipo de dato Vector

---
+++ clase_tres_py.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 13 19:44:51 2021
+
+@author: cammac
+"""
+
+
+# Tipos de Colecciones
+
+# Listas o vectores
+# Tipos de datos mutable y ordenado
+
+a = [2, 3, 4]
+b = [2, True, 'Hola', 3.4]
+c = [2, [3, 4], ['Hola', 'Mundo'], [2.3, [2.4, 2.5], 2.5, 2.6]]
+
+for valor in a:
+    print(valor)
+
+for valor in b:
+    print(valor)
+
+for valor in c:
+    print(valor)
+
+a[0] = 7
+print(b[2])
+print(c[2][1])
+print(c[3][1][1])
+
+a.append(5)  # Agrega el elemento al final de la lista
+a.remove(3)  # Elimina el elemento que coincida con el valor
+a.pop()  # Elimina el ultimo elemento del vector
+a.pop(2)  # Elimina un elemento por posición
+a.clear()  # Elimina todos los elementos del vector
+# del a
+4 in a  # Busca el elemento 4 dentro de a
+len(a)  # Cantidad de elementos del vector
+a = b  # Asignacion de b en el mismo espacio de memoria de a
+id(a)  # Convierte a decimal la dirección en memoria de un objeto
+a = b.copy()  # Copia los elementos de b en a
+a = b[:]
+b = a[0:3]
+b = a[:6]
+b = a[2:]
