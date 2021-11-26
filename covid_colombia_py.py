2d459cb7efe5bbc5b5c629e2efaf771e0d30f201
Mac Perez <macjorian.perez57@gmail.com>
2021-11-26 11:39:15 -0500

    Clase Pandas Matplotlib y covid

---
+++ covid_colombia_py.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 09:19:16 2021
+
+@author: cammac
+"""
+
+
+import pandas as pd
+
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url)
+
+# tamaño del dataset
+data.shape
+
+# columnas del dataset
+
+data.columns
+
+# acceder a una columna
+
+data.Sexo
+
+data['Sexo']
+
+# agrupar por columnas
+
+data.groupby('Sexo').size()
+
+#  Reemplazar
+
+data.Sexo.replace('f', 'F', inplace = True)
+data.Sexo.replace('m', 'M', inplace = True)
+
+# agrupar por valor 
+
+data.Estado.value_counts()
+
+# normalizar el estado
+
+data.Estado.replace('leve', 'Leve', inplace = True)
+data.Estado.replace('LEVE', 'Leve', inplace = True)
+
+# diga cuantas mujeres han fallecido en colombia por covid 19
+
+data[(data.Sexo == 'F') & (data.Estado == 'Fallecido')].shape[0]
+
+# diga cuantas mujeres han fallecido en colombia por covid 19
+
+data[(data.Sexo == 'M') & (data.Estado == 'Fallecido')].shape[0]
+
+# cantidad de hombres y mujeres fallecidas
+data[(data.Estado == 'Fallecido')].groupby('Sexo').size()
+
+# cuantos fallecidos hay en colombia
+
+data[(data.Estado == 'Fallecido')].shape[0]
+
+
+data.groupby(['Estado', 'Sexo']).size()
+
+data.groupby(['Sexo', 'Estado']).size()
+
+
+data.Estado.value_counts().plot.bar()
+
+data.Sexo.value_counts().plot.bar()
+
+data['Nombre municipio'].value_counts().head(5)
+
+
+# Cuantas mujeres fallecieron en barranquilla
+data[(data.Sexo == 'F') & (data.Estado == 'Fallecido') & (data['Nombre municipio'] == 'BARRANQUILLA')].shape[0]
+
+
+barranquilla = data[data['Nombre municipio'] == 'BARRANQUILLA']
+
+barranquilla.groupby(['Sexo', 'Estado']).size()
+
+barranquilla[barranquilla.Sexo == 'F'].Estado.value_counts().plot.bar()
+
+barranquilla.Estado.value_counts().sort_values(ascending = False)
+
+barranquilla.groupby(['Estado', 'Sexo']).size().plot.bar()
+
+
+data['fecha reporte web'] = pd.to_datetime(data['fecha reporte web'])
+barranquilla['fecha reporte web'] = pd.to_datetime(barranquilla['fecha reporte web'])
+
+data.groupby('fecha reporte web').size().cumsum().plot()
+barranquilla.groupby('fecha reporte web').size().cumsum().plot()
+
+
+data['Nombre municipio'].value_counts()
+
+
+
